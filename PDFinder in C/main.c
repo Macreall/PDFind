7a23f4f485921a9de3294bae8067e22621602da4
Macreall <gabking02@gmail.com>
2025-12-16 20:16:46 -0900

    Add PDF search functionality and UI updates

--- PDFinder in C/main.c
+++ PDFinder in C/main.c
@@ -18,6 +18,12 @@
 #include <stdio.h>
 #include "resources/stb_image.h"
 
+#include <windows.h>
+
+
+void find_pdf_data(const char* text) {
+    OutputDebugString(text);
+}
 
 void draw_background(GLuint bg_tex, int window_w, int window_h)
 {
@@ -44,7 +50,7 @@ void draw_background(GLuint bg_tex, int window_w, int window_h)
 
     glDisable(GL_TEXTURE_2D);
 
-    glPopMatrix();
+    glPopMatrix(); // MODELVIEW
     glMatrixMode(GL_PROJECTION);
     glPopMatrix();
     glMatrixMode(GL_MODELVIEW);
@@ -53,22 +59,31 @@ void draw_background(GLuint bg_tex, int window_w, int window_h)
 int main() {
     SDL_Init(SDL_INIT_VIDEO);
 
+
+
     SDL_Window* win = SDL_CreateWindow("PDFinder",
         SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-        1400, 800,
-        SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN);
+        0, 0,
+        SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN | SDL_WINDOW_FULLSCREEN_DESKTOP);
+
 
     SDL_GLContext gl_context = SDL_GL_CreateContext(win);
 
-    
     struct nk_context* ctx = nk_sdl_init(win);
 
-   
+
     struct nk_font_atlas* atlas;
     nk_sdl_font_stash_begin(&atlas);
-    struct nk_font* font = nk_font_atlas_add_default(atlas, 14, 0);
+    struct nk_font* georgia =
+        nk_font_atlas_add_from_file(
+            atlas,
+            "../assets/georgia.ttf",
+            20,          // font size
+            NULL
+        );
     nk_sdl_font_stash_end();
-    nk_style_set_font(ctx, &font->handle);
+    nk_style_set_font(ctx, &georgia->handle);
+
 
     struct nk_color background = nk_rgb(28, 48, 62);
     struct nk_color textColor = nk_rgb(255,255,255);
@@ -95,7 +110,8 @@ int main() {
     int selectedComboBox = 0;
 
     // Tabs
-    int active_tab = 0; // 0 = Tab1, 1 = Tab2
+    int active_tab = 0;
+    int search_button = 0;
 
 
     // Background Image
@@ -120,8 +136,6 @@ int main() {
 
 
     while (running) {
-        float nkWinSize = 600;
-
         SDL_Event evt;
         nk_input_begin(ctx);
         while (SDL_PollEvent(&evt)) {
@@ -130,17 +144,32 @@ int main() {
             if (evt.type == SDL_KEYDOWN && evt.key.keysym.scancode == SDL_SCANCODE_ESCAPE) {
                 running = 0;
             }
+
         }
         nk_input_end(ctx);
 
-        draw_background(bg_tex, 1400, 800);   
+        int win_w, win_h;
+        SDL_GetWindowSize(win, &win_w, &win_h);
+
+        draw_background(bg_tex, win_w, win_h);
+
+
+        // if (nk_begin(ctx, "PDFinder", nk_rect(1000, 50, 200, 250),
+        //     NK_WINDOW_BORDER | NK_WINDOW_MOVABLE | NK_WINDOW_TITLE | NK_CURSOR_RESIZE_TOP_LEFT_DOWN_RIGHT)) {
+        //     printf("here");
+        // }
 
 
 
-        
-        if (nk_begin(ctx, "Left Side", nk_rect(50, 50, nkWinSize, 700),
-        NK_WINDOW_MOVABLE | NK_CURSOR_RESIZE_TOP_LEFT_DOWN_RIGHT))
-        {
+
+        /* ---- GUI ---- */
+        if (nk_begin(ctx, "Left Side", nk_rect(50, 0, 600, 1000),
+        NK_WINDOW_MOVABLE | NK_CURSOR_RESIZE_TOP_LEFT_DOWN_RIGHT)) {
+
+
+
+
+
             // // Layout for row and column
             // nk_layout_row_static(ctx, 40, 120, 1);
             //
@@ -179,7 +208,6 @@ int main() {
             ctx->style.button.text_hover = nk_rgb(0,0,0);
 
 
-
             ctx->style.tab.padding.x = 0;
             ctx->style.tab.padding.y = 5;
             ctx->style.tab.rounding = 900.f;
@@ -191,23 +219,44 @@ int main() {
 
 
             // Tabs
-            nk_layout_row_static(ctx, 30, nkWinSize / 4, 3); 
+            float cols[] = {140, 50, 140, 50, 140};
+            nk_layout_row(ctx, NK_STATIC, 45, 5, cols);
+            ctx->style.button.rounding = 15.0f;
+
+
             if (nk_button_label(ctx, "Job Tickets")) active_tab = 0;
+            nk_spacing(ctx, 1);
             if (nk_button_label(ctx, "Invoices")) active_tab = 1;
+            nk_spacing(ctx, 1);
             if (nk_button_label(ctx, "Parts Receipts")) active_tab = 2;
 
 
-            
-            nk_layout_row_dynamic(ctx, 300, 1); 
+
+            // Tabs
+            nk_layout_row_dynamic(ctx, 300, 1);
             if (active_tab == 0) {
                 if (nk_group_begin(ctx, "Tab1", NK_WINDOW_BORDER)) {
-                    nk_layout_row_dynamic(ctx, 30, 1);
-                    nk_label(ctx, "This is Tab 1 content", NK_TEXT_LEFT);
-                    nk_checkbox_label(ctx, "Check me", &active);
+                    nk_layout_row_static(ctx, 60, 400, 1);
+
+
+                    const char *text1 = "                        You can search by customer's name, "
+                                        "                                                or Job Ticket #";
+
+                    nk_label_wrap(ctx, text1);
 
                     // Entry
-                    nk_layout_row_static(ctx, 30, 220, 1); 
-                    nk_edit_string_zero_terminated(ctx, NK_EDIT_SIMPLE, buffer, sizeof(buffer)-1, nk_filter_default);
+                    nk_layout_row_static(ctx, 30, 220, 1);
+                    if (nk_edit_string_zero_terminated(ctx, NK_EDIT_FIELD, buffer, sizeof(buffer)-1, nk_filter_default) &&
+                        nk_input_is_key_pressed(&ctx->input, NK_KEY_ENTER))
+                    {
+                        find_pdf_data(buffer);
+                    }
+
+                    // This is where we do the search algorithm to find pdf data
+                    if (nk_button_label(ctx, "Search")) {
+                        find_pdf_data(buffer);
+                    }
+
 
                     nk_group_end(ctx);
                 }
@@ -218,7 +267,7 @@ int main() {
                     nk_slider_float(ctx, 0.0f, &value, 1.0f, 0.01f);
 
                     // Entry
-                    nk_layout_row_static(ctx, 30, 220, 1); 
+                    nk_layout_row_static(ctx, 30, 220, 1);
                     nk_edit_string_zero_terminated(ctx, NK_EDIT_SIMPLE, buffer, sizeof(buffer)-1, nk_filter_default);
 
                     nk_group_end(ctx);
@@ -231,7 +280,7 @@ int main() {
                     nk_slider_float(ctx, 0.0f, &value, 1.0f, 0.01f);
 
                     // Entry
-                    nk_layout_row_static(ctx, 30, 220, 1); 
+                    nk_layout_row_static(ctx, 30, 220, 1);
                     nk_edit_string_zero_terminated(ctx, NK_EDIT_SIMPLE, buffer, sizeof(buffer)-1, nk_filter_default);
 
                     nk_group_end(ctx);
@@ -255,7 +304,7 @@ int main() {
 
 
         if (nk_begin(ctx, "Right side",
-            nk_rect(800, 50, 400, 700),
+            nk_rect(830, 0, 650, 1000),
             NK_WINDOW_BORDER | NK_WINDOW_NO_SCROLLBAR))
         {
             nk_button_label(ctx, "Right side");
@@ -266,7 +315,7 @@ int main() {
 
 
 
-        
+        /* DRAW */
         nk_sdl_render(NK_ANTI_ALIASING_ON);
         SDL_GL_SwapWindow(win);
     }
