cfd50d553d979cf44a50571060f94cd8b1631fad
Macreall <gabking02@gmail.com>
2026-02-14 15:59:31 -0900

    Update renderPDF function signature and add variables
    Added new parameters to renderPDF function for page management.

--- PDFinder in C/src/PDF Parser.h
+++ PDFinder in C/src/PDF Parser.h
@@ -5,6 +5,16 @@
 #include <commctrl.h>
 #include <stdbool.h>
 #include <stdio.h>
+#include <Shlwapi.h>
+#pragma comment(lib, "Shlwapi.lib")
 #include "../mupdf/include/mupdf/fitz.h"
 
-void renderPDF(HDC hdc, HWND hwnd, fz_context* pdf_ctx, fz_document* doc);
+
+extern int scrollY;
+extern int maxScroll;
+extern int totalPages;
+extern int page_height;
+
+
+
+void renderPDF(HDC hdc, HWND hwnd, fz_context* pdf_ctx, fz_document* doc, int current_pdf_page, int total_pages);
